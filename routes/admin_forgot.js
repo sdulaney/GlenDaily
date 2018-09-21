c57e9afbc0576a9bb2aa5879838df1cf7bd3f928
Bryce <antiteal@gmail.com>
2018-09-21 12:47:01 -0700

    fix stuffs

--- routes/admin_forgot.js
+++ routes/admin_forgot.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('admin_forgot-password', { title: 'Express' });
+  res.render('admin_forgot-password', { title: 'Glendaily Forgot Password' });
 });
 
 module.exports = router;
--- routes/admin_login.js
+++ routes/admin_login.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('admin_login', { title: 'Express' });
+  res.render('admin_login', { title: 'Glendaily Login' });
 });
 
 module.exports = router;
